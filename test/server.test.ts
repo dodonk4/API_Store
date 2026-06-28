0d0bd489c3459f6ed39d5cf611626fcb731409e7
dodonk4 <ismaelmadarieta49@gmail.com>
2026-06-28 01:13:22 -0300

    test folder structure

--- test/server.test.ts
+++ test/server.test.ts
@@ -11,3 +11,38 @@ describe('Servidor', () => {
     });
   });
 });
+
+// /auth
+  // POST /auth/refresh
+  // POST /auth/register
+  // POST /auth/login
+  // POST /auth/logout
+
+// /productos
+  // GET /productos
+  // POST /productos
+  // GET /productos:id
+  // PUT /productos:id
+  // DELETE /productos:id
+  
+// /usuarios
+  // GET /usuarios
+  // POST /usuarios
+  // GET /usuarios:id
+  // PUT /usuarios:id
+  // DELETE /usuarios:id
+
+// /ordenes
+  // GET /ordenes
+  // POST /ordenes
+  // GET /ordenes:id
+  // PUT /ordenes:id
+  // DELETE /ordenes:id 
+
+  // GET /ordenes/:ordenId/products/:ordenProductoI
+  // GET /ordenes/:ordenId/products
+  // PUT /ordenes/:ordenId/products
+  // DELETE /ordenes/:ordenId/products/:ordenProductoId
+  // POST /ordenes/:ordenId/products
+
+  
