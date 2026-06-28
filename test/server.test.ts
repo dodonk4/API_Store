154a6243eeac15e9404df9a051a66acd0874f14b
dodonk4 <ismaelmadarieta49@gmail.com>
2026-06-28 13:10:46 -0300

    test files

--- test/server.test.ts
+++ test/server.test.ts
@@ -6,43 +6,8 @@ describe('Servidor', () => {
     const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
-    expect(response.body).toEqual({
+    expect(response9.body).toEqual({
       message: 'API de Tienda',
     });
   });
 });
-
-// /auth
-  // POST /auth/refresh
-  // POST /auth/register
-  // POST /auth/login
-  // POST /auth/logout
-
-// /productos
-  // GET /productos
-  // POST /productos
-  // GET /productos:id
-  // PUT /productos:id
-  // DELETE /productos:id
-  
-// /usuarios
-  // GET /usuarios
-  // POST /usuarios
-  // GET /usuarios:id
-  // PUT /usuarios:id
-  // DELETE /usuarios:id
-
-// /ordenes
-  // GET /ordenes
-  // POST /ordenes
-  // GET /ordenes:id
-  // PUT /ordenes:id
-  // DELETE /ordenes:id 
-
-  // GET /ordenes/:ordenId/products/:ordenProductoI
-  // GET /ordenes/:ordenId/products
-  // PUT /ordenes/:ordenId/products
-  // DELETE /ordenes/:ordenId/products/:ordenProductoId
-  // POST /ordenes/:ordenId/products
-
-  
